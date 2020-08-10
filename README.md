c9f4cf50c15aea978a98423ddae75467ebb09c88
Breyner Rojas <brojas@bancolombia.com.co>
2020-08-10 11:06:51 -0500

    Initial configuration for Linux Redhat's machines

---
+++ README.md
@@ -0,0 +1,16 @@
+# FiveM AMI Packer
+
+Proyecto creado con packer y bash. El propósito es crear un template que permita el despliegue de una máquina personalizada, que mantenga levantado un servicio con Supervisord para hostear un servidor en https://fivem.net/
+
+## Changelog
+
+### v0.1 - Configuración inicial para Linux
+- Archivo de prueba. Descarga y configuración de server y cfx-dataserver par
+- Aprovisionamiento de Supervisord
+
+## Fivem
+Se utilizaron los binarios de Fivem con el propósito de levantar el servidor y la creación de estas AMI
+- https://docs.fivem.net/docs/server-manual/setting-up-a-server/
+
+## Créditos
+- Breyner a.k.a Tavet (AtolonRot)
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#/bin/sh
+
+yum update -y
+yum install git -y
+yum install python -y
+yum install python-pip -y
+pip install supervisor
+iptables --flush
+usermod -aG wheel ec2-user
+
+cd /home/ec2-user
+mkdir fxserver
+cd fxserver
+mkdir server
+mkdir server-data
+wget -P /home/ec2-user/fxserver/server http://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/2802-a203581ad20c36ab2f8688bbf2ec8660cbb8e1c9/fx.tar.xz
+cd /home/ec2-user/fxserver/server && tar -xf fx.tar.xz
+cd /home/ec2-user/fxserver/server-data
+git clone https://github.com/citizenfx/cfx-server-data /home/ec2-user/fxserver/server-data
+
+# Supervisor
+mkdir -p /etc/supervisor/conf.data
+echo_supervisord_conf > /etc/supervisor/supervisord.conf
+
+sed ';files = relative/directory/*.ini/a \
+[include]\n
+files=conf.d/*.conf' /etc/supervisor/supervisord.conf
+
+cat > /etc/systemd/system/supervisord.service << EOF
+[Unit]
+Description=Supervisor daemon
+Documentation=http://supervisord.org
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+ExecStop=/usr/local/bin/supervisorctl $OPTIONS shutdown
+ExecReload=/usr/local/bin/supervisorctl $OPTIONS reload
+KillMode=process
+Restart=on-failure
+RestartSec=42s
+
+[Install]
+WantedBy=multi-user.target
+Alias=supervisord.service
+EOF
+
+cat > /etc/supervisor/conf.d/fivem_script.conf << EOF
+[program:fivem_script]
+directory=/home/ec2-user/fxserver/server-data
+command=/home/ec2-user/fxserver/server/run.sh +exec server.cfg
+autostart=true
+autorestart=true
+
+stderr_logfile=/var/log/fivem/fivem_err.log
+stdout_logfile=/var/log/fivem/fivem_out.log
+EOF
+
+mkdir /var/log/fivem
+
+supervisorctl reread
+supervisorctl update
+systemctl enable supervisorctl
+systemctl restart supervisord.service
